cb9d31ed7af26d95293a74c80696aa5bd107c801
Ryan <Ryan@Ryans_Laptop.dhcp.nd.edu>
2017-09-27 17:49:43 -0400

    idk

--- src/calculator/SimpleCalculator.java
+++ src/calculator/SimpleCalculator.java
@@ -4,25 +4,21 @@ public class SimpleCalculator implements ICalculator {
 
 	@Override
 	public double add(double a, double b) {
-		// TODO Auto-generated method stub
 		return a + b;
 	}
 
 	@Override
 	public double sub(double a, double b) {
-		// TODO Auto-generated method stub
 		return a - b;
 	}
 
 	@Override
 	public double div(double a, double b) {
-		// TODO Auto-generated method stub
 		return a / b;
 	}
 
 	@Override
 	public double mult(double a, double b) {
-		// TODO Auto-generated method stub
 		return a * b;
 	}
 
